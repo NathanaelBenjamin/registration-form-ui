fb703ebc44b8f2501723dc848195eeae166a1477
Nathanael Benjamin <oluwakayodenathanael@gmail.com>
2022-06-18 17:10:28 +0100

    Form validation added.

---
+++ script.js
@@ -0,0 +1,56 @@
+const form = document.querySelector(".form");
+const formInput = form.querySelectorAll(".form .form-input");
+const submitButton = form.querySelector(".form .submit-btn .btn");
+const feedBack = form.querySelector(".feedback")
+
+const validation = (formList) => {
+    formList.forEach(inputItem => {
+        const input = inputItem.querySelector("input");
+        const small = inputItem.querySelector("small");
+        if(!input.value){
+            small.style.display = "flex";
+            input.classList.add("error");
+            feedBack.style.display = "none";
+        }
+
+        else if(input.placeholder === "Password" && input.value.length < 4){
+            small.innerText = 'Password must be more than 4 characters.';
+            small.style.display = "flex";
+            feedBack.style.display = "none";
+        }
+
+        else{
+            input.classList.add("success");
+            small.style.display = 'none';
+            feedBack.style.display = "flex";
+        }
+    })
+}
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    validation(formInput);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const fname = form.querySelector(".fname");
+// const lname = form.querySelector(".lname");
+// const email = form.querySelector(".email");
+// const password = form.querySelector(".password");

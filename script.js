a6f8d969daa76e669397de2c32b523c4a4616e39
Nathanael Benjamin <oluwakayodenathanael@gmail.com>
2022-06-18 19:03:27 +0100

    Form validation completed.

--- script.js
+++ script.js
@@ -7,21 +7,25 @@ const validation = (formList) => {
     formList.forEach(inputItem => {
         const input = inputItem.querySelector("input");
         const small = inputItem.querySelector("small");
+        const error = inputItem.querySelector(".error-icon");
         if(!input.value){
             small.style.display = "flex";
             input.classList.add("error");
             feedBack.style.display = "none";
+            error.style.display = "inline";
         }
 
         else if(input.placeholder === "Password" && input.value.length < 4){
             small.innerText = 'Password must be more than 4 characters.';
             small.style.display = "flex";
+            error.style.display = "inline";
             feedBack.style.display = "none";
         }
 
         else{
             input.classList.add("success");
             small.style.display = 'none';
+            error.style.display = "none";
             feedBack.style.display = "flex";
         }
     })
